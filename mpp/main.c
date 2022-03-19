7324709e6ec4a76e5b85279c3b154c21eeb89856
Jon Lara <jon.laratrigo@gmail.com>
2022-03-19 20:07:56 +0100

    Initial Commit

---
+++ mpp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mpp
+//
+//  Created by Jon Lara trigo on 19/3/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
