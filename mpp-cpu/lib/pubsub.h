c6682c18d12ce0c115e2f7221a5d00935edc780e
Jon Lara <jon.laratrigo@gmail.com>
2022-03-22 09:55:52 +0100

    Pubsub

---
+++ mpp-cpu/lib/pubsub.h
@@ -0,0 +1,35 @@
+//
+//  pubsub.h
+//  mpp-cpu
+//
+//  Created by Jon Lara trigo on 22/3/22.
+//
+
+#ifndef pubsub_h
+#define pubsub_h
+
+#include <stdbool.h>
+
+typedef enum {
+    NONE_PUBSUB_TOPIC = 0,
+    DATA_BUS_TOPIC
+} PubSubTopic;
+
+typedef struct {
+    PubSubTopic topic;
+    void *value;
+} PubSubMessage;
+
+typedef void (*on_message)(PubSubMessage);
+
+typedef struct {
+    unsigned int id;
+    PubSubTopic topic;
+    on_message on_message_fn;
+} PubSubSubscription;
+
+PubSubSubscription *subscribe_to(PubSubTopic topic, on_message on_message_fn);
+bool unsubscribe_for(PubSubSubscription *sub);
+int publish_message_to(PubSubTopic topic, void *value);
+
+#endif /* pubsub_h */
--- mpp-cpu/main.c
+++ mpp-cpu/main.c
@@ -9,13 +9,13 @@
 #include "lib/utils.h"
 #include "lib/components/components.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    
-    char *hex = int_to_hex(999999999);
-    
-    printf("Hello, World! %s\n", hex);
-    init_components();
-    return 0;
-}
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    printf("Hello, World!\n");
+//    
+//    char *hex = int_to_hex(999999999);
+//    
+//    printf("Hello, World! %s\n", hex);
+//    init_components();
+//    return 0;
+//}
