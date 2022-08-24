f0580a0275c7555f7a313aace52f4ed567cc7333
Jon Lara <jon.laratrigo@gmail.com>
2022-08-24 16:34:14 +0200

    _docs: generate index

--- _docs/astro.config.mjs
+++ _docs/astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(), // Enable React for the Algolia search component.
-    react(),
-    mdx(),
-  ],
-  site: `https://mpp-docs.nullx.me`,
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), mdx(), sitemap()],
+  site: `https://mpp-docs.nullx.me`
 });
