1794aa3574e9f5cada699a046e3ddfdc78db1d73
Jon Lara <jon.laratrigo@gmail.com>
2024-03-01 12:37:37 +0100

    Automatic build

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+set -e # exit on error
+
 _PREFIX=mpp
 CORE_CONTAINER_NAME="${_PREFIX}-core"
 FRONT_CONTAINER_NAME="${_PREFIX}-front"
-docker build -t $CORE_CONTAINER_NAME -f Dockerfile.build.core .
+TARGET_PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64/v8,linux/386,linux/ppc64le,linux/s390x"
+REPO="nullx0/mpp-front"
+VERSION=$(<VERSION)
 
+echo "This will build and push the following image to docker hub: $REPO:$VERSION"
+
+# build core
+docker build -t $CORE_CONTAINER_NAME -f Dockerfile.build.core .
 docker cp $(docker create --rm $CORE_CONTAINER_NAME):/mpp-cpu/output/. $FRONT_CONTAINER_NAME/src/lib/core/files
 
-docker rmi $(docker images "$CORE_CONTAINER_NAME" -a -q) -f
-
+# build front
 # --no-cache to prevent using old core/files
-docker build --no-cache -t $FRONT_CONTAINER_NAME $FRONT_CONTAINER_NAME
+docker build --no-cache -t $FRONT_CONTAINER_NAME $FRONT_CONTAINER_NAME -f $FRONT_CONTAINER_NAME/Dockerfile.build.front --load
+docker cp $(docker create --rm $FRONT_CONTAINER_NAME):/app/build/. ./mpp-front/build-out
 
-echo "Frontend built on $FRONT_CONTAINER_NAME"
-echo "Build finished"
+# build the server for all platforms
+docker buildx build --platform $TARGET_PLATFORMS --no-cache -t $FRONT_CONTAINER_NAME $FRONT_CONTAINER_NAME -f $FRONT_CONTAINER_NAME/Dockerfile.final --load
+
+# push to tag and latest
+docker tag $FRONT_CONTAINER_NAME $REPO:latest && docker push $REPO:latest
+docker tag $FRONT_CONTAINER_NAME $REPO:$VERSION && docker push $REPO:$VERSION
+
+# remove dirt
+docker rmi $(docker images "$CORE_CONTAINER_NAME" -a -q) -f &> /dev/null
+docker rmi $(docker images "$REPO:$VERSION" -a -q) -f &> /dev/null
+rm -rf ./mpp-front/build-out &> /dev/null
